3da2e2e38b5ec0489459a1f475f45087e184d339
f_teruhsia <teru_fukumoto@outlook.jp>
2019-04-09 21:29:54 +0900

    get user_id on form

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ app = Flask(__name__)
 @app.route('/', methods = ["GET" , "POST"])
 def index():
    if request.method == 'POST':
-       return render_template('index.html')
+       # Get name = "user_id" on form
+       user_id = request.form['user_id']
+       return render_template('index.html', user_id = user_id)
    else:
        return render_template('index.html')
+
 
 app.run(host="localhost")
