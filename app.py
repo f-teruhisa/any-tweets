9756f5e414aa40ff8292ea078c57367c0e52c559
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-04-10 16:34:05 +0900

    set Twitter api

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
+import tweepy
+import pandas as pd
 from flask import Flask, render_template, request, logging, Response, redirect, flash
+from config import CONFIG
+
+# Set the Twitter's Key
+CONSUMER_KEY = CONFIG["CONSUMER_KEY"]
+CONSUMER_SECRET = CONFIG["CONSUMER_SECRET"]
+ACCESS_TOKEN = CONFIG["ACCESS_TOKEN"]
+ACCESS_SECRET = CONFIG["ACCESS_SECRET"]
+
+# Tweepy auth
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+# Generate API instace
+api = tweepy.API(auth)
 
 app = Flask(__name__)
+
+
+# Twitter datas
+columns = [
+    "tweet_id",
+    "created_at",
+    "text",
+    "fav",
+    "retweets"
+]
 
 @app.route('/', methods = ["GET" , "POST"])
 def index():
---
+++ config.py
@@ -0,0 +1,7 @@
+# Twitter API config status
+CONFIG = {
+    "CONSUMER_KEY": "",
+    "CONSUMER_SECRET": "",
+    "ACCESS_TOKEN": "",
+    "ACCESS_SECRET": ""
+}
